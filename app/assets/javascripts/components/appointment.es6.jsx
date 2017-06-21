46eff728b57744f7cb0dcc3989cf2cfe6e0ae5ca
guanwill <w.guan@hotmail.com>
2017-06-22 01:15:59 +1000

    updated code for stateless functional components

--- app/assets/javascripts/components/appointment.es6.jsx
+++ app/assets/javascripts/components/appointment.es6.jsx
@@ -1,10 +1,38 @@
-class Appointment extends React.Component {
-  render () {
-    return (
-      <div className='appointment'>
-        <h3>{this.props.appointment.title}</h3>
-        <p>{formatDate(this.props.appointment.appt_time)}</p>
-      </div>
-    )
-  }
-}
+// class Appointment extends React.Component {
+//   render() {
+//     return (
+//       <div className='appointment'>
+//         <h3>{this.props.appointment.title}</h3>
+//         <p>{formatDate(this.props.appointment.appt_time)}</p>
+//       </div>
+//     )
+//   }
+// }
+
+
+// Rendering a stateless functional component , replaces the above code
+// const Appointment = (props) => {
+//     return (
+//       <div className='appointment'>
+//         <h3>{props.appointment.title}</h3>
+//         <p>{formatDate(props.appointment.appt_time)}</p>
+//       </div>
+//     )
+// }
+
+//Rendering a stateless functional component , by specifying incoming argument as props. the incoming props has appointment property coming through
+// const Appointment = ({appointment}) => { //by specifying appointment, you can remove this.appointment in return function
+//     return (
+//       <div className='appointment'>
+//         <h3>{appointment.title}</h3>
+//         <p>{formatDate(appointment.appt_time)}</p>
+//       </div>
+//     )
+// }
+
+//further simplication
+const Appointment = ({appointment}) =>
+  <div className='appointment'>
+    <h3>{appointment.title}</h3>
+    <p>{formatDate(appointment.appt_time)}</p>
+  </div>
--- app/assets/javascripts/components/appointments_list.es6.jsx
+++ app/assets/javascripts/components/appointments_list.es6.jsx
@@ -1,13 +1,37 @@
-class AppointmentsList extends React.Component {
-  render () {
-    return (
-      <div>
-        {this.props.appointments.map(function(appointment) {
-          return (
-            <Appointment appointment={appointment} key={appointment.id} />
-          )
-        })}
-      </div>
-    )
-  }
-};
+// class AppointmentsList extends React.Component {
+//   render () {
+//     return (
+//       <div>
+//         {this.props.appointments.map(function(appointment) {
+//           return (
+//             <Appointment appointment={appointment} key={appointment.id} />
+//           )
+//         })}
+//       </div>
+//     )
+//   }
+// };
+
+
+//rendering stateless functional component
+// const AppointmentsList = ({appointments}) => {
+//     return (
+//       <div>
+//         {appointments.map(function(appointment) {
+//           return (
+//             <Appointment appointment={appointment} key={appointment.id} />
+//           )
+//         })}
+//       </div>
+//     )
+// };
+
+//furhter simplified
+const AppointmentsList = ({appointments}) =>
+  <div>
+    {appointments.map(function(appointment) {
+      return (
+        <Appointment appointment={appointment} key={appointment.id} />
+      )
+    })}
+    </div>
